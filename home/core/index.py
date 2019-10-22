f0b5684958ad306a06413e21239660dd2be56abd
nghichcode <nghichcode@gmail.com>
2019-10-23 01:21:33 +0700

    FINAL

--- home/core/index.py
+++ home/core/index.py
@@ -1,31 +1,47 @@
 from pysnmp.hlapi import *
 
 # NCVN :: THISS IS MY BASELINE
-# errorIndication, errorStatus, errorIndex, varBinds = next(
-#   getCmd(
-#     SnmpEngine(),
-#     CommunityData('public', mpModel=0),
-#     UdpTransportTarget(('demo.snmplabs.com', 161)),
-#     ContextData(),
-#     # ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0))
-#     ObjectType(ObjectIdentity('1.3.6.1.2.1.1.1.0')),
-#     ObjectType(ObjectIdentity('1.3.6.1.2.1.1.5.0')),
-#     # ObjectType(ObjectIdentity('1.3.6.1.6.3.1.1.4.1.0')),
-#     ObjectType(ObjectIdentity('1.3.6.1.2.1.11.1.0')),
+g = setCmd(
+    SnmpEngine(),
+    CommunityData('public', mpModel=0),
+    UdpTransportTarget(('demo.snmplabs.com', 161)),
+    ContextData(),
+    ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0), 'Linux i386')
+  )
+print(g)
+print('--------+----------')
+print(next(g))
+print('--------+----------')
 
-#     ObjectType(ObjectIdentity('1.3.6.1.2.1.1.6.0'))
-#   )
-# )
+errorIndication, errorStatus, errorIndex, varBinds = next(
+  getCmd(
+    SnmpEngine(),
+    CommunityData('public', mpModel=0),
+    UdpTransportTarget(('demo.snmplabs.com', 161)),
+    ContextData(),
+    # ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0))
+    ObjectType(ObjectIdentity('IF-MIB', 'ifNumber', 0)),
+    # ObjectType(ObjectIdentity('IF-MIB', '1.3.6.1.2.1.2.1.0')),
+    ObjectType(ObjectIdentity('1.3.6.1.2.1.1.1.0')),# sysDescr
+    ObjectType(ObjectIdentity('1.3.6.1.2.1.1.5.0')),# sysName
+    ObjectType(ObjectIdentity('1.3.6.1.2.1.2.1.0')),# mib-ifNumber
+    # ObjectType(ObjectIdentity('1.3.6.1.6.3.1.1.4.1.0')),
+    ObjectType(ObjectIdentity('1.3.6.1.2.1.11.1.0')),
 
-# if errorIndication:
-#   print(errorIndication)
-# elif errorStatus:
-#   print('%s at %s' % (errorStatus.prettyPrint(),
-#     errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
-# else:
-#   for varBind in varBinds:
-#     print(' = '.join([x.prettyPrint() for x in varBind]))
-#     print('--------')
+    ObjectType(ObjectIdentity('1.3.6.1.2.1.1.6.0'))
+  )
+)
+
+if errorIndication:
+  print(errorIndication)
+elif errorStatus:
+  print('%s at %s' % (errorStatus.prettyPrint(),
+    errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+else:
+  for varBind in varBinds:
+    # print(' = '.join([x.prettyPrint() for x in varBind]))
+    print(varBind)
+    print('--------')
 
 # ---
 
@@ -54,8 +70,9 @@ from pysnmp.hlapi import *
 #     print(varBind)
 #     print('--------')
 
-try:
-  for (errorIndication, errorStatus, errorIndex, varBinds) in nextCmd(
+# try:
+#   for (errorIndication, errorStatus, errorIndex, varBinds) in nextCmd(
+  for (errorIndication, errorStatus, errorIndex, varBinds) in bulkCmd(
     SnmpEngine(),
     UsmUserData('usr-md5-none', 'authkey1'),
     UdpTransportTarget(('demo.snmplabs.com', 161)),
@@ -74,5 +91,3 @@ try:
 except KeyboardInterrupt:
   print('done.')
 
-def main():
-  pass
