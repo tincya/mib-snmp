fea0ca9a91101ad6dbd479e5d4810c88a3d76ed7
nghichcode <nghichcode@gmail.com>
2019-11-06 22:38:00 +0700

    v2s

--- home/core/index.py
+++ home/core/index.py
@@ -4,7 +4,7 @@ from pysnmp.hlapi import *
 g = setCmd(
     SnmpEngine(),
     CommunityData('public', mpModel=0),
-    UdpTransportTarget(('demo.snmplabs.com', 161)),
+    UdpTransportTarget(('10.0.0.1', 161)),
     ContextData(),
     ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0), 'Linux i386')
   )
@@ -70,7 +70,7 @@ else:
 #     print(varBind)
 #     print('--------')
 
-# try:
+try:
 #   for (errorIndication, errorStatus, errorIndex, varBinds) in nextCmd(
   for (errorIndication, errorStatus, errorIndex, varBinds) in bulkCmd(
     SnmpEngine(),
