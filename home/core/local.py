e29682186a03d9ee334e7760d28fcf10ebd004a7
nghichcode <nghichcode@gmail.com>
2019-11-06 23:55:04 +0700

    some new

--- home/core/local.py
+++ home/core/local.py
@@ -1,7 +1,8 @@
 from pysnmp.hlapi import *
 
 router1 = {'host':'10.0.0.1', 'port':161}
-router2 = {'host':'10.0.0.1', 'port':161}
+router2 = {'host':'demo.snmplabs.com', 'port':161}
+router = router1
 
 # g = setCmd(
 #     SnmpEngine(),
@@ -15,11 +16,21 @@ router2 = {'host':'10.0.0.1', 'port':161}
 # print(next(g))
 # print('--------+----------')
 
+"""
+Retrieved Function: 
+- Availability: hrDeviceStatus, localHwDeviceOper/AdminStatus 
+- CPU info: cpmCPUTotal5sec/ busyPer
+- Inteface Status : ifAdminStatus
+- Interface Link Status: ifOperStatus
+- Interface Bandwidth: ifInOctets, ifOutOctets, ifspeed, locIfIn/OutBitsSec
+- IP accounting: cipMacTable, cipMacSwitchedPkts, cipMacSwitchedBytes, actCheckPoint 
+- Topology ( optional)
+"""
 errorIndication, errorStatus, errorIndex, varBinds = next(
   getCmd(
     SnmpEngine(),
     CommunityData('public', mpModel=0),
-    UdpTransportTarget((router1['host'], router1['port'])),
+    UdpTransportTarget((router['host'], router['port'])),
     ContextData(),
     # ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0))
     ObjectType(ObjectIdentity('IF-MIB', 'ifNumber', 0)),
@@ -27,8 +38,21 @@ errorIndication, errorStatus, errorIndex, varBinds = next(
     ObjectType(ObjectIdentity('1.3.6.1.2.1.1.1.0')),# sysDescr
     ObjectType(ObjectIdentity('1.3.6.1.2.1.1.5.0')),# sysName
     ObjectType(ObjectIdentity('1.3.6.1.2.1.2.1.0')),# mib-ifNumber
-    # ObjectType(ObjectIdentity('1.3.6.1.6.3.1.1.4.1.0')),
     ObjectType(ObjectIdentity('1.3.6.1.2.1.11.1.0')),
+    # ObjectType(ObjectIdentity('1.3.6.1.2.1.2.2.1.7')),
+    ObjectType(ObjectIdentity('1.3.6.1.4.1.9.9.109.1.1.1.1.3.1')),
+    # ObjectType(ObjectIdentity('HOST-RESOURCES-MIB','hrDeviceStatus',1)),
+    
+    ObjectType(ObjectIdentity('IF-MIB','ifAdminStatus',1)),
+    ObjectType(ObjectIdentity('IF-MIB','ifOperStatus',1)),
+    ObjectType(ObjectIdentity('IF-MIB','ifInOctets',1)),
+    ObjectType(ObjectIdentity('IF-MIB','ifOutOctets',1)),
+    ObjectType(ObjectIdentity('IF-MIB','ifSpeed',1)),
+    ObjectType(ObjectIdentity('1.3.6.1.4.1.9.2.2.1.1.6.1')),
+    ObjectType(ObjectIdentity('1.3.6.1.4.1.9.2.2.1.1.8.1')),
+    # ObjectType(ObjectIdentity('1.3.6.1.4.1.9.9.84.1.2.1.2')),
+
+    # ObjectType(ObjectIdentity('IF-MIB','ifspeed',1)),
 
     ObjectType(ObjectIdentity('1.3.6.1.2.1.1.6.0'))
   )
@@ -40,6 +64,7 @@ elif errorStatus:
   print('%s at %s' % (errorStatus.prettyPrint(),
     errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
 else:
+  print('All data:')
   for varBind in varBinds:
     # print(' = '.join([x.prettyPrint() for x in varBind]))
     print(varBind)
